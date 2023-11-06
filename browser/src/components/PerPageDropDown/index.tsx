cfb11b6218474c95bb7e72e3e8cbd17bdc419efd
Jan Smejkal <jan.smejkal.osobne@gmail.com>
2023-11-06 07:32:56 +0100

    fix perPage when custom param passed

--- browser/src/components/PerPageDropDown/index.tsx
+++ browser/src/components/PerPageDropDown/index.tsx
@@ -1,19 +1,24 @@
-import {type SyntheticEvent, useCallback} from 'react';
+import {type SyntheticEvent, useCallback, useMemo} from 'react';
 import {usePaginationContext} from "~/contexts/paginationContext";
 
 export type SetPerPageHandler = (perPage: number) => void
 
+const perPageDefaultOptions = [10, 25, 50, 100];
+const getOptions = (perPage: number) => {
+    return perPageDefaultOptions.includes(perPage) ? perPageDefaultOptions : [...perPageDefaultOptions, perPage].sort((a,b)=> a - b)
+}
+
 const PerPageDropDown= () => {
-  const { handleSetPerPage }= usePaginationContext()
+  const { handleSetPerPage, perPage }= usePaginationContext()
+    const perPageOptions = useMemo(() => {
+        return getOptions(perPage)
+    },[perPage])
     const selectHandler = useCallback((e: SyntheticEvent<HTMLSelectElement>) => {
         handleSetPerPage(parseInt(e.currentTarget.value, 10))
     },[])
   return (<>
-    <select onChange={selectHandler} className="text-sm text-white bg-[rgba(0,0,0,.1)] rounded">
-        <option value="10">10</option>
-        <option value="20">20</option>
-        <option value="50">50</option>
-        <option value="100">100</option>
+    <select onChange={selectHandler} value={perPage} className="text-sm text-white bg-[rgba(0,0,0,.1)] rounded">
+        {perPageOptions.map((option) => <option key={option} value={option}>{option}</option>)}
     </select>
   </>);
 };
