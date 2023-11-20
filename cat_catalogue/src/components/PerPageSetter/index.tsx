d2d3befeae2d2afc3908dba5f67b4a5e17bdd40c
Jan Smejkal <jan.smejkal.osobne@gmail.com>
2023-11-20 13:42:14 +0100

    add api key verification

--- cat_catalogue/src/components/PerPageSetter/index.tsx
+++ cat_catalogue/src/components/PerPageSetter/index.tsx
@@ -1,15 +1,25 @@
 import React, {FunctionComponent, useCallback, useEffect} from 'react';
 import {useCookies} from "react-cookie";
+import useAPIKey from "../../hooks/useAPIKey.ts";
 
 interface OwnProps {}
 
 type Props = OwnProps;
 
+export const SMALL_PAGE_OPTIONS = [10]
+export const LARGE_PAGE_OPTIONS = [40,80]
 
 const PerPageSetter: FunctionComponent<Props> = () => {
+    const {hasValidAPIKey} = useAPIKey()
+
     const [cookies, setCookie] = useCookies(['per_page']);
-    const [perPage, setPerPage] = React.useState(cookies.per_page ? parseInt(cookies.per_page) : 40);
-    const [paginationOptions] = React.useState([40, 80]);
+    const [perPage, setPerPage] = React.useState(cookies.per_page ? parseInt(cookies.per_page) : 10);
+    const [paginationOptions, setPaginationOptions] = React.useState(SMALL_PAGE_OPTIONS);
+
+    useEffect(() => {
+        setPaginationOptions(hasValidAPIKey ? LARGE_PAGE_OPTIONS : SMALL_PAGE_OPTIONS)
+        setPerPage(hasValidAPIKey ? LARGE_PAGE_OPTIONS[0] : SMALL_PAGE_OPTIONS[0])
+    },[hasValidAPIKey])
 
     useEffect(() => {
         setCookie('per_page', perPage.toString())
